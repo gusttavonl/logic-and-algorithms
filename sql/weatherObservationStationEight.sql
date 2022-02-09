059406c101928ffd56fa8ef8255a9273df949472
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-02-09 09:44:28 -0300

    :sparkles: solved weather observation station eight

---
+++ sql/weatherObservationStationEight.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE (
+    CITY LIKE 'A%'
+    OR CITY LIKE 'E%'
+    OR CITY LIKE 'I%'
+    OR CITY LIKE 'O%'
+    OR CITY LIKE 'U%'
+  )
+  AND (
+    CITY LIKE '%A'
+    OR CITY LIKE '%E'
+    OR CITY LIKE '%I'
+    OR CITY LIKE '%O'
+    OR CITY LIKE '%U'
+  )
+ORDER BY CITY ASC;
