d7f05fb3b1f47d8427941385e4e3977760dbd384
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-02-21 08:42:12 -0300

    :sparkles: solved whatTypeOfTriangle

---
+++ sql/whatTypeOfTriangle.sql
@@ -0,0 +1,12 @@
+SELECT CASE
+    WHEN A = B
+    OR B = C
+    OR A = C THEN 'Isosceles'
+    WHEN A = B
+    AND B = C THEN 'Equilateral'
+    WHEN A + B <= C
+    OR A + C <= B
+    OR B + C <= A THEN 'Not A Triangle'
+    ELSE 'Scalene'
+  END
+FROM TRIANGLES;
